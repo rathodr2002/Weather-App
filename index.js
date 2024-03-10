69c2984453ee27bf5af533c2a9abe460d14babc3
rathodr2002 <rishirathod2002@gmail.com>
2024-03-10 12:43:16 +0530

    Completed Code for Javascript

--- index.js
+++ index.js
@@ -1,18 +1,65 @@
 const apiKey = "3c86c2a09b6401c1bb6cc4483cd96b22"
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=pune&appid="
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid="
 
-async function checkweather(){
+
+const searchBox = document.querySelector(" .search input")
+const searchBtn = document.querySelector(" .search button")
+const weatherIcon = document.querySelector(" .weather-icon");
+
+async function checkweather(city){
     try {
-        const response = await fetch(apiUrl + `${apiKey}`);
+        const response = await fetch(apiUrl + `${apiKey}` + `&q=${city}`);
         const data = await response.json();
-        console.log("Inside Async Function")
-        console.log(data);
+        console.log(data)
+        if(response.ok){
+            let cityName = data.name;
+            let temperature = Math.round(data.main.temp);
+            let humidity = data.main.humidity ;
+            let speed = (data.wind.speed ).toFixed(2)  ;
+            
+            if(data.weather[0].main == "Clear"){
+                weatherIcon.src = "images/clear.png"
+            }
+            else if (data.weather[0].main == "Clouds" ){
+                weatherIcon.src = "images/clouds.png"
+            }
+            else if (data.weather[0].main == "Drizzle" ){
+                weatherIcon.src = "images/drizzle.png"
+            }
+            else if (data.weather[0].main == "Mist" ){
+                weatherIcon.src = "images/mist.png"
+            }
+            else if (data.weather[0].main == "Rain" ){
+                weatherIcon.src = "images/rain.png"
+            }
+            else if (data.weather[0].main == "Snow" ){
+                weatherIcon.src = "images/snow.png"
+            }
+            else if (data.weather[0].main == "Haze" ){
+                weatherIcon.src = "images/haze.png"
+            }
+
+            document.querySelector(".weather-name").innerHTML =  data.weather[0].main;
+            document.querySelector(".city").innerHTML =  cityName;
+            document.querySelector(".temp").innerHTML = temperature + `°C`;
+            document.querySelector(".humidy").innerHTML = humidity + `%`;
+            document.querySelector(".wind").innerHTML = speed + `km/h`;
+        }
+        else {
+            weatherIcon.src = ""
+            document.querySelector(".temp").innerHTML = `0°C`;
+            document.querySelector(".city").innerHTML = "City not found!";
+            document.querySelector(".humidy").innerHTML = `0 %`;
+            document.querySelector(".wind").innerHTML = `0 km/h`;
+            console.log("City not found!")
+        }
+
     } catch (error) {
         console.log("Something went wrong : "+error);
     }
-
-    let temp = data.main.temp();
-    console.log(temp);   
 }
+searchBtn.addEventListener('click', ()=> {
+    checkweather(searchBox.value);
+})
 
-checkweather();
+
